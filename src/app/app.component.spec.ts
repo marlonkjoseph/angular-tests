d0982cb6003a7fe504830e53c8b41cbf5763c360
Marlon Joseph <marlonkjoseph@hotmail.com>
2018-10-10 22:01:51 -0400

    Added custom jasmine matchers, hidden test examples.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
+
+// My custom matcher functions
+import { toBeDisplayed, notToBeDisplayed } from './custom-matchers';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -7,21 +12,78 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    // I added custom Jasmine matchers for cleaner tests!
+    jasmine.addMatchers({ toBeDisplayed, notToBeDisplayed });
   }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+  describe('using element truthy/falsy', async() => {
+    it('should show link 1', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link1'));
+      expect(control).toBeTruthy();
+    }));
+  
+    it('should not show link 2', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link2'));
+      expect(control).toBeFalsy();
+    }));
+  
+    it('should not show link 3 (should fail but passed)', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link3'));
+
+      // Deliberately fail the test
+      expect(control).toBeTruthy();  // <--- This should fail and it didn't
+      
+      //expect(control).toBeFalsy();
+    }));
+
+    it('should not show link 3 (should fail and did)', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link3:not([hidden]')); // We can add  the :not([hidden])
+                                                                                 // selector to filter out hidden
+                                                                                 // elements. This is messy so check
+                                                                                 // out the custom Jasmine matchers
+                                                                                 // I wrote below.
+
+
+      // Deliberately fail the test
+      expect(control).toBeTruthy();  // <--- This should fail and it did
+      
+      //expect(control).toBeFalsy();
+    }));
+  });
+
+  describe('using custom jasmine matcher', async() => {
+    it('should show link 1', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link1'));
+      expect(control).toBeDisplayed();
+    }));
+  
+    it('should show link 2', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link2'));
+      expect(control).notToBeDisplayed();
+    }));
+  
+    it('should not show link 3 (should fail and did)', async(() => {
+      const fixture = TestBed.createComponent(AppComponent);
+      fixture.detectChanges();
+      const control = fixture.debugElement.query(By.css('.link3'));
+      
+      // Deliberately fail the test      
+      expect(control).toBeDisplayed();  // <--- This should fail and it did!
+
+      // expect(control).notToBeDisplayed();
+    }));
+  });
 });
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,12 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+
+// To get the TypeScript to compile my custom ambient type
+// type I declare them in the typings.d.ts file.
+declare namespace jasmine {
+  interface Matchers<T> {
+    toBeDisplayed(expectationFailOutput?: any): boolean;
+    notToBeDisplayed(expectationFailOutput?: any): boolean;
+  }
+}
